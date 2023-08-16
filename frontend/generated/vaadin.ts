1cb81d32c439f51b045728f16655dec06f25a455
Fritz Schubert <fritzschubert21@outlook.de>
2023-08-16 12:36:24 +0200

    last generated commit

--- frontend/generated/vaadin.ts
+++ frontend/generated/vaadin.ts
@@ -3,8 +3,3 @@ import './vaadin-featureflags.js';
 import './index';
 
 import 'Frontend/generated/jar-resources/vaadin-dev-tools/vaadin-dev-tools.js';
-
-import './theme-break-decider.global.generated.js';
-import {applyTheme} from './theme.js';
-
-applyTheme(document);
