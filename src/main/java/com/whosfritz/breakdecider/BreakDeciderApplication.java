bef81147adbf4602b6b48fe5a9420e191a26654d
Fritz Schubert <fritzschubert21@outlook.de>
2023-08-18 15:04:20 +0200

    added PWA

--- src/main/java/com/whosfritz/breakdecider/BreakDeciderApplication.java
+++ src/main/java/com/whosfritz/breakdecider/BreakDeciderApplication.java
@@ -1,12 +1,14 @@
 package com.whosfritz.breakdecider;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Theme("break-decider")
+@PWA(name = "Break Decider", shortName = "Break Decider")
 public class BreakDeciderApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
